ec4d5a1eaf68a3a8315518358b7ae2c03c0e3076
unknown <abdul7to7@gmail.com>
2024-07-13 11:41:37 +0530

    server added to frontend

--- frontend/expenseForm.js
+++ frontend/expenseForm.js
@@ -1,4 +1,4 @@
-const server = "http://localhost:4000";
+const server = "https://s-expensetrackerdemo2.onrender.com";
 
 //Dom Loading expenses
 let currentPage = 1;
--- frontend/login.js
+++ frontend/login.js
@@ -1,4 +1,4 @@
-const server = "http://localhost:4000";
+const server = "https://s-expensetrackerdemo2.onrender.com";
 
 document.getElementById("loginForm")?.addEventListener("submit", (e) => {
   e.preventDefault();
