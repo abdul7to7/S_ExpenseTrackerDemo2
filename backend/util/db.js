1b56c3de74bcf6af2f90bb166d8069f54f4541f5
unknown <abdul7to7@gmail.com>
2024-07-13 11:26:16 +0530

    added postgresql

--- backend/util/db.js
+++ backend/util/db.js
@@ -1,8 +1,24 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("expense_demo", "root", "root", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    logging: false,
+  }
+);
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
 
 module.exports = sequelize;
